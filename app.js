9d5d26f619901085d98ebc02f28cfbf237d8f798
OmarObaid1 <omar.kamal28@outlook.com>
2021-08-01 19:02:34 +0300

    new

---
+++ app.js
@@ -0,0 +1,12 @@
+alert("hello welcome to my website");
+let username = prompt("what's your name");
+
+document.write("welcome " + username);
+
+let age = Number(prompt("enter your age please"));
+age += 10;
+console.log(age);
+
+let username2 = prompt("who is the fav player");
+
+alert("nice choice  " + username);
